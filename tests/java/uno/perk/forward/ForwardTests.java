a50182e47a5093832a16a450adffad00cd05b679
John Sirois <john.sirois@gmail.com>
2015-11-29 10:06:22 -0700

    Kill unused import.

--- tests/java/uno/perk/forward/ForwardTests.java
+++ tests/java/uno/perk/forward/ForwardTests.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 public class ForwardTests {
